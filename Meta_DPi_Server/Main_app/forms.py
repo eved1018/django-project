52c6add69ee8b05c11a398c5d9d83e19a0960115
E_Edelstein <edelsteinevan@gmail.com>
2020-07-31 13:16:27 -0400

    commit

--- Meta_DPi_Server/Main_app/forms.py
+++ Meta_DPi_Server/Main_app/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
+
+
 class PDBForm(forms.Form):
     pdb = forms.CharField(label=False)
+
+class FileForm(forms.Form):
+    file = forms.FileField(label=False)
+
     
--- Meta_DPi_Server/Main_app/views.py
+++ Meta_DPi_Server/Main_app/views.py
@@ -3,9 +3,24 @@ from django.http import HttpResponse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from .forms import PDBForm
+from .forms import PDBForm,FileForm
+import os
+from django.conf import settings
 
 
+def handle_uploaded_file(datafile):
+    pdbs= []
+    with datafile.open('r') as f:
+        lines = f.readlines()
+        for line in lines:
+            pdb = line.decode('utf8')
+            pdbs.append(pdb)
+        f.close()
+    for pdb in pdbs:
+        pdb = pdb.rstrip("\n")
+        context = Parser(pdb)
+    return context 
+    
 
 def predition_score_get():
     # this is where we will get the three prediction scores and combine them into a dataframe with residue as index  
@@ -25,44 +40,62 @@ def Meta_DPI_Setup(pdb,chain):
     results["col2"] = ["1","2",'3']
     results = results.to_html(index=False)
     tree = '/Users/evanedelstein/Desktop/Research_Evan/Raji_Summer2019_atom/Data_Files/CrossVal_logreg_RF/5foldCV/Crossvaltest47/Trees/Rftree_CV1.svg'
-    return results,tree
+    return results, tree
+
+def Parser(pdb):
+    # parses pdb input will display error if _ or . isnt used or pdb is too big/ to short need to add lookup from RCBS 
+    error_message= ""
+
+    if len(pdb) == 4:
+        
+        chain = None
+        results,tree = Meta_DPI_Setup(pdb,chain)
+        context = {'results' : results,'tree' : tree}
+        return context
+    elif len(pdb) == 6:
+        
+        if "_" in pdb:
+            
+            pdb_chain = pdb.split("_")
+            pdb = pdb_chain[0]
+            chain = pdb_chain[1]
+            results,tree = Meta_DPI_Setup(pdb,chain)
+            context = {'results' : results,'tree' : tree}
+            return context
+            
+        if "." in pdb:
+            
+            pdb_chain = pdb.split("_")
+            pdb = pdb_chain[0]
+            chain = pdb_chain[1]
+            results,tree = Meta_DPI_Setup(pdb,chain)
+            context = {'results' : results,'tree' : tree}
+            return context
+    else: 
+        error_message = "PDb id is not known "
+        results = ""
+        context = {'results': results ,'error_message': error_message}
+        return context
 
 
 
 def home(request):
-    
-    req = request
     form = PDBForm(request.POST)
+    file_form = FileForm(request.POST)
     error_message = ""
     if request.method == "POST" and form.is_valid():
         pdb = form.cleaned_data['pdb']
-        if len(pdb) == 4:
-            chain = None
-            results,tree = Meta_DPI_Setup(pdb,chain)
-            context = {'results' : results,'tree' : tree}
-            return render(request,'Main_app/Results.html' ,context)
-        elif len(pdb) == 6:
-            if "_" in pdb:
-                pdb_chain = pdb.split("_")
-                pdb = pdb_chain[0]
-                chain = pdb_chain[1]
-                results,tree = Meta_DPI_Setup(pdb,chain)
-                context = {'results' : results,'tree' : tree}
-                return render(request,'Main_app/Results.html' ,context)
-                
-            if "." in pdb:
-                pdb_chain = pdb.split("_")
-                pdb = pdb_chain[0]
-                chain = pdb_chain[1]
-                results,tree = Meta_DPI_Setup(pdb,chain)
-                context = {'results' : results,'tree' : tree}
-                return render(request,'Main_app/Results.html' ,context)
-        else: 
-            error_message = "PDb id is not known "
+        context = Parser(pdb)
+        return render(request,'Main_app/Results.html' ,context)
+    elif request.method == 'POST' and request.FILES:
+        file_form = FileForm(request.POST, request.FILES)
+        datafile = request.FILES['file']
+        context  = handle_uploaded_file(datafile)
+        return render(request,'Main_app/Results.html' ,context)
     
     form = PDBForm
-   
-    return render(request,'Main_app/home.html',{'title':home ,'form': form , 'error_message': error_message  } )
+    file_form = FileForm
+    return render(request,'Main_app/home.html',{'title':home ,'form': form ,'form2':file_form, 'error_message': error_message  } )
 
 
     
