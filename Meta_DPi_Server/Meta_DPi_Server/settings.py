3d7bd73cfb4f60721885e1d1cab7bc94d822b57c
E_Edelstein <edelsteinevan@gmail.com>
2020-08-10 14:24:49 -0400

    commit

--- Meta_DPi_Server/Meta_DPi_Server/settings.py
+++ Meta_DPi_Server/Meta_DPi_Server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '78df=&n+-opza5x#3%7t)xqkqe4ixhvym2r2#%)f=djjd=sahc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['metadpiapp.herokuapp.com/']
 
 
 # Application definition
